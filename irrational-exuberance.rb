44e3fa7f8064de6cef5989c66bdfe6b1328e941d
David Bunn <debunn@yahoo.com>
2016-01-12 21:35:52 -0500

    First strategy - not working yet

--- irrational-exuberance.rb
+++ irrational-exuberance.rb
@@ -39,6 +39,7 @@ class StockPosition
     @purchased_total = 0 # The total amount of money spent to purchase stocks
     @sold_total = 0 # The total amount of money made selling stocks
     @price_metric = 0 # Metric for the average price paid for held/shorted stocks
+    @last_trade = 0 # Holds the last traded value we executed on
     @open_orders = [] # Stores order numbers for all placed orders
     @closed_orders = [] # Stores order numbers where order['open'] = false
     @order_log = {} # Tracks each transaction, prevents duplicate processing
@@ -98,11 +99,17 @@ class StockPosition
       @stock_sold += shares.abs
       @sold_total += (price * shares.abs)
     end
+    @last_trade = price
+    $loop_kicker = $profit * -1 # set the loop kicker to lower my asking price
     @stock_position += shares
   end
 
   def current_price_metric
     @price_metric
+  end
+
+  def last_trade
+    @last_trade
   end
 
   def open_orders
@@ -135,7 +142,7 @@ class StockPosition
   def action_processed?(order_id, ts)
     # Determine if this transaction has already been processed
     
-    if @order_log[order_id].nil? || @order_log[order_id][ts].nil?
+    if @order_log[order_id].nil? || !(@order_log[order_id].has_key?(ts))
       false
     else 
       true
@@ -236,8 +243,9 @@ end
 
 $my_pos = StockPosition.new
 $my_analysis = MarketAnalysis.new
-$profit = 125 # The minimum profit goal for a transaction
-$price_buffer = 0 # The amount above bid or below ask to charge
+$profit = 200 # The minimum profit goal for a transaction
+$price_buffer = 10 # The amount above bid or below ask to charge
+$loop_kicker = 0 # The amount to kick up my asking price (increased per loop iteration)
 
 # Set functionality for when the ticker websocket receives a new quote
 ticker_websocket = Stockfighter::Websockets.new(gm.config)
@@ -254,25 +262,31 @@ execution_websocket.add_execution_callback { |execution|
   # Ensure you don't have long running operations (eg calling api.*) as part of this
   # callback method as the event processing for all websockets is performed on 1 thread.
   
-  # Process each fill item as it is received
-  
-  execution['order']['fills'].each do |fill_item|
-    if execution['order']['direction'] == 'sell'
-      fill_item['qty'] = fill_item['qty'] * -1
+  # Process each fill item as it is received - verify execution is valid first
+
+  if execution['order']['account'] == gm.config[:account] &&
+      execution['order']['symbol'] == gm.config[:symbol] &&
+      execution['order']['venue'] == gm.config[:venue]
+
+    execution['order']['fills'].each do |fill_item|
+      if execution['order']['direction'] == 'sell'
+        fill_item['qty'] = fill_item['qty'] * -1
+      end
+
+      # Only process transactions that haven't been recorded as processed
+      if !( $my_pos.action_processed?(execution['order']['id'], fill_item['ts']) )
+        $my_pos.trade(fill_item["qty"], fill_item["price"])
+        $my_pos.record_action(execution['order']['id'], fill_item['ts'])
+      end
     end
-    
-    # Only process transactions that haven't been recorded as processed
-    if !( $my_pos.action_processed?(execution['order']['id'], fill_item['ts']) )
-      $my_pos.trade(fill_item["qty"], fill_item["price"])
-      $my_pos.record_action(execution['order']['id'], fill_item['ts'])
+
+    # Resolve the trade if it is no longer open
+
+    if !(execution['order']['open'])
+      p 'Order: ' + (execution['order']['id']).to_s + ' is closed.'
+      $my_pos.close_order(execution['order']['id'])
     end
-  end
-    
-  # Resolve the trade if it is no longer open
-  
-  if !(execution['order']['open'])
-    p 'Order: ' + (execution['order']['id']).to_s + ' is closed.'
-    $my_pos.close_order(execution['order']['id'])
+
   end
   
 }
@@ -282,9 +296,9 @@ ticker_thr = Thread.new { ticker_websocket.start(tickertape_enabled:true, execut
 execution_thr = Thread.new { execution_websocket.start(tickertape_enabled:false, executions_enabled:true) }
 
 # Main processing loop - stops when the game state is not active
-while gm.active? do
+while true do
   $order_id = 0
-  take_action = {action: 'sleep'}
+  take_action = {action: 'sleep', order_type: 'immediate-or-cancel'}
   last_quote = $my_analysis.last_quote
   last_quote = last_quote.nil? ? api.get_quote : last_quote
 
@@ -296,49 +310,36 @@ while gm.active? do
       order_status = api.order_status(order_id)
       order_status['open'] ? true : $my_pos.close_order(order_id)
     end
-  elsif $my_pos.current_position < -300 # too far on margin - buy some stock (at cost if necessary)
+  elsif $my_pos.current_position < 0 # too far on margin - buy some stock (at cost if necessary)
     if $my_analysis.last_ask < $my_pos.current_price_metric
-      take_action = {action: 'buy', amount: 200, price: $my_analysis.last_ask}
+      take_action = {action: 'buy', amount: 200, price: ($my_pos.last_trade - $profit)}
     end
 
-  elsif $my_pos.current_position > 300 # too long - sell some stock (at cost if necessary)
+  elsif $my_pos.current_position > 600 # too long - sell some stock (at cost if necessary)
     if $my_analysis.last_bid > $my_pos.current_price_metric
-      take_action = {action: 'sell', amount: 200, price: $my_analysis.last_bid}
+      take_action = {action: 'sell', amount: 600, price: ($my_pos.last_trade + $profit)}
     end
 
-  elsif $my_pos.current_price_metric == 0 # no stock position - buy some stock
-    take_action = {action: 'buy', amount: 20, price: last_quote['last']}
+  elsif $my_pos.current_position == 0 # no stock position - buy some stock
+    take_action = {action: 'buy', amount: 200, price: ($my_analysis.last_ask + $price_buffer)}
 
-  elsif sell_profit > buy_profit && sell_profit > 0 # if selling is more profitable, try selling
-    if $my_analysis.last_bid > ($my_pos.current_price_metric) + ($profit + $price_buffer)
-      volume = $my_pos.current_position < 0 ? 50 : (sell_profit > 400 ? 400 : sell_profit)
-      take_action = {action: 'sell', amount: volume, price: ($my_analysis.last_bid - $price_buffer)}
-    end
+  elsif $my_pos.profit > 7000000 # Attempt to pop the bubble
+    $my_pos.execute_trade(-2000, ($my_pos.last_trade * 0.1), api, 'limit')
+    p 'Selling 2000@' + ($my_pos.last_trade * 0.1).to_s
 
-  elsif buy_profit >= sell_profit && buy_profit > 0 # if buying is more profitable, try buying
-    if $my_analysis.last_ask < ($my_pos.current_price_metric) - ($profit + $price_buffer)
-      volume = $my_pos.current_position > 0 ? 50 : (buy_profit > 400 ? 400 : buy_profit)
-      take_action = {action: 'buy', amount: volume , price: ($my_analysis.last_ask + $price_buffer)}
-    end
-
-  else # Try a crazy buy at the lowest known price, or sell at the highest (after they vary by $5)
-    if $my_analysis.highest_last - $my_analysis.lowest_last > 5
-      $my_pos.current_position < 0 ?
-          (take_action = {action: 'buy', amount: 300, price: $my_analysis.lowest_last}) : true
-      $my_pos.current_position > 0 ?
-          (take_action = {action: 'sell', amount: 300, price: $my_analysis.highest_last}) : true
-    end
+  else # Try a buy based on my last trade, plus a loop based increase
+    take_action = {action: 'buy', amount: 50, price: ($my_pos.last_trade + $loop_kicker)}
   end
 
   # Execute any action assigned in this loop, otherwise skip this turn
   case take_action[:action]
     when 'buy'
-      $my_analysis.bid_volatility > 3 && $my_pos.current_position > 0 ? true :
-          $my_pos.execute_trade(take_action[:amount], take_action[:price], api, 'immediate-or-cancel')
+      $my_pos.current_position > 0 ? true :
+          $my_pos.execute_trade(take_action[:amount], take_action[:price], api)
           p 'Buying ' + take_action[:amount].to_s + '@' + take_action[:price].to_s
     when 'sell'
-      $my_analysis.ask_volatility < -3 && $my_pos.current_position < 0 ? true :
-          $my_pos.execute_trade((take_action[:amount]) * -1, take_action[:price], api, 'immediate-or-cancel')
+      $my_pos.current_position < 0 ? true :
+          $my_pos.execute_trade((take_action[:amount]) * -1, take_action[:price], api)
           p 'Selling ' + take_action[:amount].to_s + '@' + take_action[:price].to_s
   end
 
@@ -349,5 +350,7 @@ while gm.active? do
         ', Price metric: ' + $my_pos.current_price_metric.to_s
 
   $last_output == tick_output ? true : (p tick_output ; $last_output = tick_output)
+
+  $loop_kicker += 25
 
 end
